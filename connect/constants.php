aa7c0cfcbdf1a05d28cab48618584b9ca0edb091
sila <petersila2022@gmail.com>
2024-08-21 15:13:32 +0300

    hide

--- connect/constants.php
+++ connect/constants.php
@@ -1,8 +1,5 @@
 <?php
 
-define('EMAIL', 'petersila2002@gmail.com');
-define('PASSWORD', 'efxx jhvf nvdg kxom');
-
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
