b7ccbfbecf85788f118826db8618b50d1fe5940e
sila <petersila2022@gmail.com>
2024-08-21 15:57:15 +0300

    non

--- connect/constants.php
+++ connect/constants.php
@@ -1,8 +1,18 @@
 <?php
+require("dotenv");
 
-define('DB_HOST', 'localhost');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', 'root');
+// define('DB_NAME', 'travelkenya');
+
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'travelkenya');
+define('DB_PASS', 'xQDQfJezSiJTGMKBjVJsdlKGUQpsPBEv');
+define('DB_NAME', 'railway');
+define('PORT', '3306');
+
+
+
 
 ?>
